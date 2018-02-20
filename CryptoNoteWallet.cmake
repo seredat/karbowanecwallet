ab490a79bf335ac66fc633b465d9ef8ccccca66b
Frank <frank@getfrank.be>
2018-02-19 17:05:15 -0800

    Case sensitivity

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,4 +1,4 @@
-set(CN_PROJECT_NAME "befrank")
-set(CN_CURRENCY_DISPLAY_NAME "BeFrank")
+set(CN_PROJECT_NAME "befrank-wallet-qt")
+set(CN_CURRENCY_DISPLAY_NAME "BeFrank Wallet")
 set(CN_CURRENCY_TICKER "BFR")
 set(CN_VERSION 1.0.1)
