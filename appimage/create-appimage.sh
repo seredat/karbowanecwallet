38b0d55e8e68c04b3875773e6237ba579888e911
Aiwe <maksymivv@gmail.com>
2021-11-01 19:16:30 +0200

    Do we need to copy binary??

--- appimage/create-appimage.sh
+++ appimage/create-appimage.sh
@@ -7,8 +7,7 @@ if ! test -f "linuxdeploy-plugin-qt-x86_64.AppImage"; then
     wget https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage
 fi
 chmod u+x ./*.AppImage
-cp ../build/release/KarbowanecWallet .
 mkdir -p usr/share/icons
 mkdir -p usr/lib
 cp ../src/images/karbowanez.png usr/share/icons/karbowanec.png
-./linuxdeploy-x86_64.AppImage --executable ./KarbowanecWallet --desktop-file karbowanecwallet.desktop --appdir . --output appimage --plugin qt
+./linuxdeploy-x86_64.AppImage --executable ../build/release/KarbowanecWallet --desktop-file karbowanecwallet.desktop --appdir . --output appimage --plugin qt
