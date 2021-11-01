f3bdc8892a970a4c1fef4f191e7ed6d11c8756fc
Aiwe <maksymivv@gmail.com>
2021-11-01 19:42:46 +0200

    Include translations

--- appimage/create-appimage.sh
+++ appimage/create-appimage.sh
@@ -9,5 +9,7 @@ fi
 chmod u+x ./*.AppImage
 mkdir -p usr/share/icons
 mkdir -p usr/lib
+mkdir -p opt/karbo
 cp ../src/images/karbowanez.png usr/share/icons/karbowanec.png
+cp ../build/release/languages/*.qm opt/karbo/
 ./linuxdeploy-x86_64.AppImage --executable ../build/release/KarbowanecWallet --desktop-file karbowanecwallet.desktop --appdir . --output appimage --plugin qt
