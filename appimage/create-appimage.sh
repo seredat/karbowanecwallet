096bc542eaa1778633a447a37201e9712e77a006
aiwe <maksymivv@gmail.com>
2026-03-17 19:58:07 -0500

    Fix appimage

--- appimage/create-appimage.sh
+++ appimage/create-appimage.sh
@@ -1,15 +1,46 @@
 #!/bin/bash
+set -e
 
-if ! test -f "linuxdeploy-x86_64.AppImage"; then
+APP=KarbowanecWallet
+APPDIR=AppDir
+
+# Download tools
+if ! test -f linuxdeploy-x86_64.AppImage; then
     wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
 fi
-if ! test -f "linuxdeploy-plugin-qt-x86_64.AppImage"; then
+
+if ! test -f linuxdeploy-plugin-qt-x86_64.AppImage; then
     wget https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage
 fi
-chmod u+x ./*.AppImage
-mkdir -p usr/share/icons
-mkdir -p usr/lib
-mkdir -p usr/share/karbo/languages
-cp ../src/images/karbowanez.png usr/share/icons/karbowanec.png
-cp ../build/release/languages/*.qm usr/share/karbo/languages/
-./linuxdeploy-x86_64.AppImage --executable ../build/release/KarbowanecWallet --desktop-file ../src/karbowanecwallet.desktop --appdir . --output appimage --plugin qt
+
+chmod +x *.AppImage
+
+# Clean AppDir
+rm -rf $APPDIR
+mkdir -p $APPDIR/usr/bin
+mkdir -p $APPDIR/usr/share/icons/hicolor/256x256/apps
+mkdir -p $APPDIR/usr/share/applications
+mkdir -p $APPDIR/usr/share/karbo/languages
+
+# Copy binary
+cp ../build/release/$APP $APPDIR/usr/bin/
+
+# Copy desktop file
+cp ../src/karbowanecwallet.desktop $APPDIR/usr/share/applications/
+
+# Copy icon
+cp ../src/images/karbowanez.png \
+   $APPDIR/usr/share/icons/hicolor/256x256/apps/karbowanec.png
+
+# Copy translations
+cp ../build/release/languages/*.qm \
+   $APPDIR/usr/share/karbo/languages/
+
+# Build AppImage
+./linuxdeploy-x86_64.AppImage \
+  --appdir $APPDIR \
+  --executable $APPDIR/usr/bin/$APP \
+  --desktop-file $APPDIR/usr/share/applications/karbowanecwallet.desktop \
+  --icon-file $APPDIR/usr/share/icons/hicolor/256x256/apps/karbowanec.png \
+  --plugin qt \
+  --output appimage
--- src/TranslatorManager.cpp
+++ src/TranslatorManager.cpp
@@ -15,78 +15,94 @@ TranslatorManager* TranslatorManager::m_Instance = 0;
 TranslatorManager::TranslatorManager()
 {
     QString lang = Settings::instance().getLanguage();
-    if(lang.isEmpty()) {
+    if (lang.isEmpty()) {
         lang = QLocale::system().name();
-        lang.truncate(lang.lastIndexOf('_'));
+        lang = lang.section('_', 0, 0); // cleaner truncate
     }
 
 #if defined(_MSC_VER)
-    m_langPath = QApplication::applicationDirPath();
-    m_langPath.append("/languages");
+    m_langPath = QApplication::applicationDirPath() + "/languages";
+
 #elif defined(Q_OS_MAC)
-    m_langPath = QApplication::applicationDirPath();
-    m_langPath = m_langPath + "/../Resources/languages/";
-#elif defined(__FreeBSD__)
-    m_langPath = "/usr/local/share/karbo/karbowallet/languages";
+    m_langPath = QApplication::applicationDirPath() + "/../Resources/languages/";
+
 #else
-    m_langPath = "/opt/karbo/languages";
-#endif
-    // Workaround for AppImage
-    if (nullptr != std::getenv("APPIMAGE") && !QDir("m_langPath").exists()) {
-        m_langPath = "../share/karbo/languages";
+    // Default: try relative to executable (works for AppImage + local builds)
+    QString basePath = QApplication::applicationDirPath();
+
+    QString appImagePath = basePath + "/../share/karbo/languages";
+    QString localPath    = basePath + "/languages";
+    QString systemPath   = "/usr/share/karbo/languages";
+
+    if (QDir(appImagePath).exists()) {
+        m_langPath = QDir(appImagePath).absolutePath();
+    } else if (QDir(localPath).exists()) {
+        m_langPath = QDir(localPath).absolutePath();
+    } else {
+        m_langPath = systemPath;
     }
+#endif
+
+    qDebug() << "Translation path:" << m_langPath;
 
     QDir dir(m_langPath);
 
     QStringList resources = dir.entryList(QStringList("??.qm"));
-    for (int j = 0; j < resources.size(); j++)
+    for (const QString& file : resources)
     {
-        QString locale = resources[j];
+        QString locale = file;
         locale.truncate(locale.lastIndexOf('.'));
+
         if (locale == lang)
         {
-            QTranslator* pTranslator = new QTranslator;
-            if (pTranslator->load(resources[j], m_langPath))
+            QTranslator* translator = new QTranslator;
+            if (translator->load(file, m_langPath))
             {
-                qApp->installTranslator(pTranslator);
+                qApp->installTranslator(translator);
                 m_keyLang = locale;
-                m_translators.insert(locale, pTranslator);
+                m_translators.insert(locale, translator);
                 break;
             }
         }
     }
+
+    // Qt translations
     QString langPathSys;
-#if !defined(_MSC_VER) && !defined(Q_OS_MAC)
-#if defined(__FreeBSD__)
-    langPathSys = "/usr/local/share/qt5/translations";
-#else
-    langPathSys = "/usr/share/qt5/translations";
-#endif
-    QDir dirSys(langPathSys);
-    QStringList Listfilter;
-    Listfilter << "qt_??.qm" << "qtbase_??.qm" << "qtscript_??.qm" << "qtscript_??.qm";
-    Listfilter << "qtquick1_??.qm" << "qtmultimedia_??.qm" << "qtxmlpatterns_??.qm";
-    QStringList resourcesQt = dirSys.entryList(Listfilter);
-#else
+
+#if defined(_MSC_VER) || defined(Q_OS_MAC)
     langPathSys = m_langPath;
-    QDir dirSys(langPathSys);
-    QStringList resourcesQt = dirSys.entryList(QStringList("qt_??.qm"));
+#else
+    QString qtAppImagePath = QApplication::applicationDirPath() + "/../translations";
+    QString qtSystemPath   = "/usr/share/qt6/translations";
+
+    if (QDir(qtAppImagePath).exists()) {
+        langPathSys = qtAppImagePath;
+    } else {
+        langPathSys = qtSystemPath;
+    }
 #endif
-    for (int j = 0; j < resourcesQt.size(); j++)
+
+    QDir dirSys(langPathSys);
+
+    QStringList filters;
+    filters << "qt_??.qm" << "qtbase_??.qm" << "qtmultimedia_??.qm";
+
+    QStringList resourcesQt = dirSys.entryList(filters);
+
+    for (const QString& file : resourcesQt)
     {
-        const int langLen = 2;
-        QString locale = resourcesQt[j];
+        QString locale = file;
         locale.truncate(locale.lastIndexOf('.'));
-        QString l = locale;
-        l.remove(0, l.length() - langLen);
-        if (l == lang)
+
+        QString shortLang = locale.right(2);
+
+        if (shortLang == lang)
         {
-            QTranslator* qTranslator = new QTranslator;
-            if (qTranslator->load(resourcesQt[j], langPathSys))
+            QTranslator* translator = new QTranslator;
+            if (translator->load(file, langPathSys))
             {
-                qApp->installTranslator(qTranslator);
-                m_keyLang = locale;
-                m_translators.insert(locale, qTranslator);
+                qApp->installTranslator(translator);
+                m_translators.insert(locale, translator);
             }
         }
     }
